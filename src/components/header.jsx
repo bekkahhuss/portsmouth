24f8515b2db3d399eb0aa2934828d18031b3649f
bekkahhuss <bekkahhuss@gmail.com>
2022-05-18 21:47:45 -0400

    added Navigation Bar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,19 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+
 export const Header = () => {
     return (
-        <h1>Header</h1>
-    )
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Rebekkah Huss</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#about">About Me</Nav.Link>
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#resume">Resume</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    );
 }
 
 
